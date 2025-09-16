b627a848b379e604356e583645a3fb6339255b44
Samuel <henrisamuel234@gmail.com>
2025-09-16 17:05:07 -0300

    Parcial 1(SEM TESTE)

--- src/main.py
+++ src/main.py
@@ -8,5 +8,23 @@ chave = int(input("Digite a chave: "))
 #inicializar variavel da mensagem final
 mensagem_final = ""
 
+#percorrer cada caractere da mensagem
+for caractere in mensagem:
+    letra = caractere #letra original
+    chave = 0 #quantidade de posicoes que a letra vai andar
 
+    posicao = ord(letra) - ord('a') #achar a posicao da letra no alfabeto (0-25)
+    nova_posicao = (posicao + chave) % 26 #aplica o deslocamento(para criptografar)
+    nova_letra = chr(nova_posicao + ord('a')) #converte a nova posicao de volta para letra
 
+    if opcao_user == 1:
+        nova_posicao = (posicao + chave) %26 #aplica o deslocamento(para criptografar)
+    elif opcao_user == 2:
+        nova_posicao = (posicao - chave) %26 #aplica o deslocamento(para descriptografar)
+
+    nova_posicao = nova_posicao + mensagem_final #adiciona a nova letra na mensagem final
+
+else:
+    letra += mensagem_final #adiciona a letra original na mensagem final
+
+print("Mensagem final:", mensagem_final)
