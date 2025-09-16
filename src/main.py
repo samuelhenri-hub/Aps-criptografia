75a04d30dd552c85d3436be782438c28d53bfbf8
Samuel <henrisamuel234@gmail.com>
2025-09-16 14:20:26 -0300

    Estrutura de escolha user

--- src/main.py
+++ src/main.py
@@ -1,4 +1,32 @@
-#MENU PARA ESCOLHA DO USARIO#
-
+#MENU PARA ESCOLHA DO USARIO
 print("------CRIPTOGRAFIA------\n")
-OpcaoUser = int(input(" 1 - Criptografar\n 2 - Descriptografar\n 3 - Sair"))
+opcaoUser = int(input(" 1 - Criptografar\n 2 - Descriptografar\n 3 - Sair"))
+
+#CRIPTOGRAFIA
+if opcaoUser == 1:
+    criptografia = string(input("Digite a mensagem que deseja ser criptografada: "))
+    chave = int(input("Digite a chave de criptografia: "))
+    mensagemCriptografada = ""
+    #FALTA COMPLETAR A CRIPTOGRAFIA
+
+#DESCRIPTOGRAFIA
+elif opcaoUser == 2:
+    chave = int(input("Digite a chave para descriptografar: "))
+    print(f"A mensagem é: {} ")
+    #FALTA COMPLETAR A DESCRIPTOGRAFIA
+
+#SAIR
+elif opcaoUser == 3:
+    print("------SAINDO DO PROGRAMA-----")
+    exit()
+
+#OPCAO INVALIDA
+else: 
+    print("OPÇÃO INVALIDA")
+    retorno = int(input("Deseja voltar ao menu?\n 1 - sim 2 - não"))
+    if retorno == 1:
+        # Voltar ao menu
+        opcaoUser = int(input(" 1 - Criptografar\n 2 - Descriptografar\n 3 - Sair"))
+    else:
+        exit()
+
