530cfba9ca59f09a89764525f30f21f24083fc6d
Samuel <henrisamuel234@gmail.com>
2025-09-16 15:43:05 -0300

    Caracteres aceitos

--- src/main.py
+++ src/main.py
@@ -1,26 +1,35 @@
 #MENU PARA ESCOLHA DO USARIO
 print("------CRIPTOGRAFIA------\n")
 opcaoUser = int(input(" 1 - Criptografar\n 2 - Descriptografar\n 3 - Sair"))
+#lista de caracteres valídos
+letra = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+
 
 #CRIPTOGRAFIA
 if opcaoUser == 1:
-    criptografia = string(input("Digite a mensagem que deseja ser criptografada: "))
+    frase = string(input("Digite a mensagem que deseja ser criptografada: "))
     chave = int(input("Digite a chave de criptografia: "))
-    mensagemCriptografada = ""
+
+    for letra in frase:
+
+
     #FALTA COMPLETAR A CRIPTOGRAFIA
 
 #DESCRIPTOGRAFIA
+
 elif opcaoUser == 2:
     chave = int(input("Digite a chave para descriptografar: "))
     print(f"A mensagem é: {mensagemDescriptografada}")
     #FALTA COMPLETAR A DESCRIPTOGRAFIA
 
 #SAIR
+
 elif opcaoUser == 3:
     print("------SAINDO DO PROGRAMA-----")
     exit()
 
 #OPCAO INVALIDA
+
 else: 
     print("OPÇÃO INVALIDA")
     retorno = int(input("Deseja voltar ao menu?\n 1 - sim 2 - não"))
